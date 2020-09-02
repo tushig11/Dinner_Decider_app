72d01e57daf9b528490288e4e27805ff39f9c06d
tushig11 <tushig11@gmail.com>
2020-09-02 13:02:19 -0500

    1. UI Design added 2. Add Food and Decide ( Click event added )

--- app/src/main/java/com/example/dinnerdeciderapp/MainActivity.kt
+++ app/src/main/java/com/example/dinnerdeciderapp/MainActivity.kt
@@ -1,2 +1,28 @@
 package com.example.dinnerdeciderapp
 
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlin.random.Random
+
+class MainActivity : AppCompatActivity() {
+    var choices = arrayListOf<String>("Hamburger", "Pizza", "Mexican", "American", "Chinese")
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        print(choices.size)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun addBtnClick(view: View){
+        choices.add(foodInput.text.toString())
+        foodOutput.text = choices[choices.size-1]
+        print(choices.size)
+    }
+
+    fun decideBtnClick(view: View){
+        val randomChoice = Random.nextInt(0,choices.size)
+        foodOutput.text = choices[randomChoice]
+    }
+}
